727fe700eb3e25accf9ab6a2dc3a8152fd7725bc
Mugunthan-ofl <mugunthan.r2024cse@sece.ac.in>
2025-12-04 09:33:10 +0530

    day 3 task completed

---
+++ Project1/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+// import FlexBoxComponent from "./flex-box/flex-box-component";
+// import HelloWorldComponent from "./hello-world/hello-world-component";
+// import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
+//import PlacementTrainingComponent from "./plcement_training/placement-training-component";
+//import PositionComponent from "./positioning/position-component";
+import TrustedbyComponent from "./trusted-by/trusted-by-components";
+import { GetInTouchStyle } from "./get-in-touch/get-in-touch-style";
+import GetInTouchStyleComponent from "./get-in-touch/get-in-touch-component";
+function App() {
+  return (
+    <div>
+      {/* <HelloWorldComponent /> */}
+      {/* <FlexBoxComponent /> */}
+      {/* <LeadersTomorrowComponent /> */}
+      {/* <SymbolOfExcellenceComponent/> */}
+      {/* <PositionComponent /> */}
+      {/* <TrustedbyComponent /> */}
+      <GetInTouchStyleComponent/>
+    </div>
+  );
+}
+
+export default App;
